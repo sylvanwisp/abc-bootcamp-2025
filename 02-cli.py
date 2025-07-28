8453872c6607a808bfce6dd2337e2b325d28cdbb
sylvanwisp <byookgoh@gmail.com>
2025-07-28 11:25:20 +0900

    add initial python files & requirements.txt

---
+++ 02-cli.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv() # .env 파일이 있다면, 환경변수로서 로딩
+
+import os
+API_KEY = os.environ["OPENAI_API_KEY"]
+
+from openai import OpenAI
+client = OpenAI(api_key=API_KEY)  # OPENAI_API_KEY 환경변수 지정이 필요
+
+response = client.responses.create(
+    model="gpt-4o",
+    input="Entail the significance of the number 42 in relation to 'The Hitchhiker's Guide to the Galaxy'.",
+)
+
+print("usage :", response.usage)
+print(response.output_text)
